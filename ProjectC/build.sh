d3480072d14095e4749dd0bdcaa76d30610aefdf
Nick Manos <nmanos@student.bridgew.edu>
2021-11-15 18:45:01 -0500

    Fix build.sh and add diskc.img

--- ProjectC/build.sh
+++ ProjectC/build.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env sh
 
 bcc -0 -ansi -c -o ./build/kernel_c.o ./src/c/kernel.c
-ld86 -o kernel -d ./build/kernel_c.o ./src/asm/kernel_asm.o
+as86 ./src/asm/kernel.asm -o ./build/kernel_asm.o
+ld86 -o kernel -d ./build/kernel_c.o ./build/kernel_asm.o
 # dd if=kernel of=diskc.img bs=512 conv=notrunc seek=3
 ./loadFile kernel
 ./loadFile ./diskc/message.txt
