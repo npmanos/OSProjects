de7bc30969b2bf7820f4f4239e3af106c3bd8424
Nick Manos <nmanos@student.bridgew.edu>
2021-12-21 00:50:31 -0500

    Refactor and clean up
    Replace mod() function with macro
    
    Replace '\0' and "\r\n" with macros
    
    Remove unused functions and variables
    
    Replace direct function calls in kernel with 0x21 interrupts

--- ProjectD/src/c/kernel.c
+++ ProjectD/src/c/kernel.c
@@ -5,6 +5,9 @@
     kernel.c
 */
 
+#define CRLF "\r\n"
+#define TERM '\0'
+
 void printChar(char);
 void printString(char *);
 void readString(char *);
@@ -12,7 +15,7 @@ void readSector(char *, int);
 void writeSector(char *, int);
 void readFile(char *, char *, int *);
 void writeFile(char *, char *, int);
-void deleteFile(char*);
+void deleteFile(char *);
 void executeProgram(char *);
 void terminate();
 
@@ -20,25 +23,16 @@ char shell[6];
 
 void main()
 {
-    // char buffer[13312];
-    // int sectorsRead = 0;
     shell[0] = 's';
     shell[1] = 'h';
     shell[2] = 'e';
     shell[3] = 'l';
     shell[4] = 'l';
-    shell[5] = '\0';
+    shell[5] = TERM;
 
     makeInterrupt21();
     interrupt(0x10, 0x3, 0, 0, 0); /* clear screen by setting video mode */
-
-    // STEP 1
-    // interrupt(0x21, 3, "messag", buffer, &sectorsRead);
-    // if (sectorsRead > 0) {
-    //     interrupt(0x21, 0, buffer, 0, 0);
-    // } else {
-    //     interrupt(0x21, 0, "File \"messag\" not found.\r\n");
-    // }
+    interrupt(0x21, 0, "COMP 350 OS vD\r\n\r\n", 0, 0);
 
     //STEP 4
     // interrupt(0x21, 8, "this is a test message", "tstmg", 3);
@@ -48,74 +42,83 @@ void main()
     // while (1);
 }
 
-void printChar(char c) {
+void printChar(char c)
+{
     interrupt(0x10, 0xe * 256 + c, 0, 0, 0);
 }
 
-void printString(char *str) {
-    while (*str != '\0')
+void printString(char *str)
+{
+    while (*str != TERM)
     {
         printChar(*str);
         str++;
     }
 }
 
-void readString(char *str) {
+void readString(char *str)
+{
     int i = 0;
     while (i < 4096)
     {
         int c = interrupt(0x16, 0, 0, 0, 0);
-        switch (c) {
-            case 0xd:
-                *str = c;
-                str++;
-                *str = 0xa;
-                str++;
-                *str = '\0';
-                printChar(c);
-                printChar(0xa);
-                i++;
-                return;
-            case 0x8:
-                if (i > 0) 
-                {
-                    printChar(0x8);
-                    printChar(' ');
-                    printChar(0x8);
-                    str--;
-                    i--;
-                }
-                break;
-            default:
-                *str = c;
-                printChar(c);
-                str++;
-                i++;
-                break;
+        switch (c)
+        {
+        case 0xd:
+            *str = c;
+            str++;
+            *str = 0xa;
+            str++;
+            *str = TERM;
+            printChar(c);
+            printChar(0xa);
+            i++;
+            return;
+        case 0x8:
+            if (i > 0)
+            {
+                printChar(0x8);
+                printChar(' ');
+                printChar(0x8);
+                str--;
+                i--;
+            }
+            break;
+        default:
+            *str = c;
+            printChar(c);
+            str++;
+            i++;
+            break;
         }
     }
 }
 
-void readSector(char* buffer, int sector) {
+void readSector(char *buffer, int sector)
+{
     interrupt(0x13, (2 << 8) + 1, buffer, sector + 1, 0x80);
 }
 
-void writeSector(char* buffer, int sector) {
+void writeSector(char *buffer, int sector)
+{
     interrupt(0x13, (3 << 8) + 1, buffer, sector + 1, 0x80);
 }
 
-void readFile(char* filename, char* buffer, int* sectorsRead) {
+void readFile(char *filename, char *buffer, int *sectorsRead)
+{
     char dir[512];
     int entry;
     int offset;
     int match;
     *sectorsRead = 0;
 
-    readSector(&dir, 2);
+    // readSector(&dir, 2);
+    interrupt(0x21, 0x2, dir, 2, 0);
 
-    for (entry = 0; entry < 512; entry = entry + 32) {
+    for (entry = 0; entry < 512; entry += 32)
+    {
         match = 0;
-        for (offset = 0; offset < 6 && dir[entry + offset] != '\0'; offset++)
+        for (offset = 0; offset < 6 && dir[entry + offset] != TERM; offset++)
         {
             if (dir[entry + offset] != filename[offset])
             {
@@ -126,12 +129,14 @@ void readFile(char* filename, char* buffer, int* sectorsRead) {
             match = 1;
         }
 
-        if (match) {
+        if (match)
+        {
             break;
         }
     }
 
-    if (!match) {
+    if (!match)
+    {
         return;
     }
 
@@ -143,70 +148,79 @@ void readFile(char* filename, char* buffer, int* sectorsRead) {
     }
 }
 
-void writeFile(char* buffer, char* filename, int size) {
-    char map[512];
-    char dir[512];
-    int byteSize = 512 * size;
-    char zBuf[512];
+void writeFile(char *buffer, char *filename, int size)
+{
+    char map[512], dir[512], zBuf[512];
     int sectors[255];
-    int entry;
-    int offset;
-    int sector;
-    int toWrite;
-    int i;
-    int j;
-    int bufSize = sizeof(buffer);
+    int entry, offset, sector, i, j, z;
 
-    readSector(&map, 1);
-    readSector(&dir, 2);
+    // readSector(&map, 1);
+    interrupt(0x21, 2, map, 1, 0);
+    // readSector(&dir, 2);
+    interrupt(0x21, 2, dir, 2, 0);
 
-    for (entry = 0; entry < 512 && dir[entry] != '\0'; entry += 32);
+    for (entry = 0; entry < 512 && dir[entry] != TERM; entry += 32);
 
-    if (entry == 512) {
+    if (entry == 512)
+    {
         return;
     }
 
-    for (sector = 3, j = 0; sector < 512 && j < size; sector++) {
-        if (map[sector] == '\0') {
+    for (sector = 3, j = 0; sector < 512 && j < size; sector++)
+    {
+        if (map[sector] == TERM)
+        {
             sectors[j++] = sector;
         }
     }
 
-    if (j < size) {
+    if (j < size)
+    {
         return; // not enough free sectors
     }
 
-    for (offset = 0; *filename != '\0'; offset++, filename++)
+    for (offset = 0; *filename != TERM; offset++, filename++)
     {
         dir[entry + offset] = *filename;
     }
 
-    for (; offset < 6; offset++) {
-        dir[entry + offset] = '\0';
+    for (; offset < 6; offset++)
+    {
+        dir[entry + offset] = TERM;
     }
 
-    for (i = 0; i < size && offset < 32; i++, offset++, buffer += 512) {
-        writeSector(zBuf, sectors[i]);
-        writeSector(buffer, sectors[i]);
+    for (z = 0; z < 512; z++) {
+        zBuf[z] = 0;
+    }
+
+    for (i = 0; i < size && offset < 32; i++, offset++, buffer += 512)
+    {
+        // writeSector(zBuf, sectors[i]);
+        interrupt(0x21, 6, zBuf, sectors[i], 0);
+        // writeSector(buffer, sectors[i]);
+        interrupt(0x21, 6, buffer, sectors[i], 0);
         map[sectors[i]] = 0xFF;
         dir[entry + offset] = sectors[i];
     }
-    
-    writeSector(&map, 1);
-    writeSector(&dir, 2);
+
+    // writeSector(&map, 1);
+    interrupt(0x21, 6, map, 1, 0);
+    // writeSector(&dir, 2);
+    interrupt(0x21, 6, dir, 2, 0);
 }
 
-void deleteFile(char* filename) {
-    char map[512];
-    char dir[512];
-    int entry;
-    int offset;
-    int match;
+void deleteFile(char *filename)
+{
+    char map[512], dir[512];
+    int entry, offset, match;
 
-    readSector(map, 1);
-    readSector(dir, 2);
+    // readSector(map, 1);
+    interrupt(0x21, 2, map, 1, 0);
+    // readSector(dir, 2);
+    interrupt(0x21, 2, dir, 2, 0);
 
-    for (entry = 0; entry < 512; entry = entry + 32) {
+    for (entry = 0; entry < 512; entry = entry + 32)
+    {
         match = 0;
         for (offset = 0; offset < 6; offset++)
         {
@@ -219,35 +233,43 @@ void deleteFile(char* filename) {
             match = 1;
         }
 
-        if (match) {
+        if (match)
+        {
             break;
         }
     }
 
-    if (!match) {
+    if (!match)
+    {
         return;
     }
 
-    dir[entry] = '\0';
+    dir[entry] = TERM;
 
     for (entry, offset; dir[entry + offset] != 0; offset++)
     {
-        map[dir[entry + offset]] = '\0';
+        map[dir[entry + offset]] = TERM;
     }
 
-    writeSector(map, 1);
-    writeSector(dir, 2);
+    // writeSector(map, 1);
+    interrupt(0x21, 6, map, 1, 0);
+    // writeSector(dir, 2);
+    interrupt(0x21, 6, dir, 2, 0);
+
 }
 
-void executeProgram(char* progName) {
+void executeProgram(char *progName)
+{
     char buf[13312];
     int sectorsRead = 0;
     int i;
 
     interrupt(0x21, 3, progName, buf, &sectorsRead);
 
-    if (sectorsRead > 0) {
-        for (i = 0; i < 512 * sectorsRead; i++) {
+    if (sectorsRead > 0)
+    {
+        for (i = 0; i < 512 * sectorsRead; i++)
+        {
             putInMemory(0x2000, i, buf[i]);
         }
 
@@ -255,48 +277,50 @@ void executeProgram(char* progName) {
     }
 }
 
-void terminate() {
+void terminate()
+{
     shell[0] = 's';
     shell[1] = 'h';
     shell[2] = 'e';
     shell[3] = 'l';
     shell[4] = 'l';
-    shell[5] = '\0';
+    shell[5] = TERM;
     interrupt(0x21, 4, shell, 0, 0);
 }
 
 void handleInterrupt21(int ax, int bx, int cx, int dx)
 {
-    switch (ax) {
-        case 0x0:
-            printString(bx);
-            break;
-        case 0x1:
-            readString(bx);
-            break;
-        case 0x2:
-            readSector(bx, cx);
-            break;
-        case 0x3:
-            readFile(bx, cx, dx);
-            break;
-        case 0x4:
-            executeProgram(bx);
-            break;
-        case 0x5:
-            terminate();
-            break;
-        case 0x6:
-            writeSector(bx, cx);
-            break;
-        case 0x7:
-            deleteFile(bx);
-            break;
-        case 0x8:
-            writeFile(bx, cx, dx);
-            break;
-        default:
-            printString("ERROR! Invalid instruction\0");
-            break;
+    switch (ax)
+    {
+    case 0x0:
+        printString(bx);
+        break;
+    case 0x1:
+        readString(bx);
+        break;
+    case 0x2:
+        readSector(bx, cx);
+        break;
+    case 0x3:
+        readFile(bx, cx, dx);
+        break;
+    case 0x4:
+        executeProgram(bx);
+        break;
+    case 0x5:
+        terminate();
+        break;
+    case 0x6:
+        writeSector(bx, cx);
+        break;
+    case 0x7:
+        deleteFile(bx);
+        break;
+    case 0x8:
+        writeFile(bx, cx, dx);
+        break;
+    default:
+        printString("ERROR! Invalid instruction\0");
+        break;
     }
 }
--- ProjectD/src/c/shell.c
+++ ProjectD/src/c/shell.c
@@ -8,14 +8,14 @@
 #define COM_NUM 7
 #define CRLF "\r\n"
 #define TERM '\0'
+#define mod(A, B) A - (A / B * B)
 
 void print(char *);
 void printLn(char *);
 void readLn(char *);
-int mod(int, int);
 
 void getArg(char *, char *);
-void file(char *);
+
 void type(char *);
 void exec(char *);
 void dir();
@@ -23,8 +23,9 @@ void del(char *);
 void copy(char *);
 void create(char *);
 
-void main() {
-    char *arg;
+void main()
+{
+    char input[4096];
     char *commands[COM_NUM];
     char *typeStr = "type";
     char *execStr = "exec";
@@ -32,11 +33,8 @@ void main() {
     char *delStr = "del";
     char *copyStr = "copy";
     char *createStr = "create";
-    char *inCom;
-    int com;
-    int match;
-    char *prMatch;
-    int c;
+    int com, match, c;
+    char *inCom, *arg;
     commands[0] = CRLF;
     commands[1] = typeStr;
     commands[2] = execStr;
@@ -45,17 +43,15 @@ void main() {
     commands[5] = copyStr;
     commands[6] = createStr;
 
-    printLn("COMP 350 OS vD.6\r\n");
-
-    while (1) {
-        char input[4096];
+    while (1)
+    {
         print("/$ ");
         readLn(input);
 
         for (com = 0; com < COM_NUM; com++)
         {
-            match = 0; 
-            for (inCom = input, c = 0; *inCom != ' ' && commands[com][c] != '\0'; inCom++, c++)
+            match = 0;
+            for (inCom = input, c = 0; *inCom != ' ' && commands[com][c] != TERM; inCom++, c++)
             {
                 if (*inCom != commands[com][c])
                 {
@@ -74,101 +70,111 @@ void main() {
             }
         }
 
-        switch (com) {
-            case 0:
-                break;
-            case 1:
-                getArg(inCom, arg);
-                type(++arg);
-                print(CRLF);
-                break;
-            case 2:
-                getArg(inCom, arg);
-                exec(++arg);
-                print(CRLF);
-                break;
-            case 3:
-                dir();
-                break;
-            case 4:
-                getArg(inCom, arg);
-                del(++arg);
-                break;
-            case 5:
-                getArg(inCom, arg);
-                copy(++arg);
-                break;
-            case 6:
-                getArg(inCom, arg);
-                create(++arg);
-                break;
-            default:
-                print("ERROR! Command not found: ");
-                print(input);
-                break;
-            }
+        switch (com)
+        {
+        case 0:
+            break;
+        case 1:
+            getArg(inCom, arg);
+            type(++arg);
+            print(CRLF);
+            break;
+        case 2:
+            getArg(inCom, arg);
+            exec(++arg);
+            print(CRLF);
+            break;
+        case 3:
+            dir();
+            break;
+        case 4:
+            getArg(inCom, arg);
+            del(++arg);
+            break;
+        case 5:
+            getArg(inCom, arg);
+            copy(++arg);
+            break;
+        case 6:
+            getArg(inCom, arg);
+            create(++arg);
+            break;
+        default:
+            print("ERROR! Command not found: ");
+            print(input);
+            break;
+        }
 
         print(CRLF);
     }
 }
 
-void print(char* str) {
+void print(char *str)
+{
     syscall(0, str, 0, 0);
 }
 
-void printLn(char* str) {
+void printLn(char *str)
+{
     syscall(0, str, 0, 0);
-    syscall(0, "\r\n", 0, 0);
+    syscall(0, CRLF, 0, 0);
 }
 
-void getArg(char* input, char* arg) {
+void readLn(char *buf)
+{
+    syscall(1, buf, 0, 0);
+}
 
-    for (; *input != '\r' && *input != '\0'; arg++, input++)
+void getArg(char *input, char *arg)
+{
+
+    for (; *input != '\r' && *input != TERM; arg++, input++)
     {
         *arg = *input;
     }
 
-    *arg = '\0';
+    *arg = TERM;
 }
 
-void readLn(char* buf) {
-    syscall(1, buf, 0, 0);
-}
-
-int mod(int a, int b) {
-    return a - (a / b * b);
-}
-
-void type(char* file) {
+void type(char *file)
+{
     char buf[13312];
     int sectors;
 
     syscall(3, file, buf, &sectors);
 
-    if (sectors > 0) {
+    if (sectors > 0)
+    {
         print(buf);
-    } else {
+    }
+    else
+    {
         print("Cannot type '");
         print(file);
         print("': No such file");
     }
 }
 
-void exec(char* program) {
+void exec(char *program)
+{
     char buf[13312];
     int sectors;
 
     syscall(3, program, buf, &sectors);
 
-    if (sectors > 0) {
+    if (sectors > 0)
+    {
         syscall(4, program, 0, 0);
-    } else {
+    }
+    else
+    {
         print("ERROR! Program not found: ");
         printLn(program);
     }
 }
 
-void dir() {
+void dir()
+{
     char dir[512];
     char filename[7];
     char sizeStr[3];
@@ -177,42 +183,50 @@ void dir() {
     int s;
 
     syscall(2, dir, 2, 0);
-    for (entry = 0; entry < 512; entry += 32) {
-        if (dir[entry] == '\0') {
+    for (entry = 0; entry < 512; entry += 32)
+    {
+        if (dir[entry] == TERM)
+        {
             continue;
         }
 
-        for (offset = 0; offset < 6 && dir[entry + offset] != '\0'; offset++) {
+        for (offset = 0; offset < 6 && dir[entry + offset] != TERM; offset++)
+        {
             filename[offset] = dir[entry + offset];
         }
 
-        filename[offset] = '\0';
+        filename[offset] = TERM;
 
-        for (offset = 6, s = 0; offset < 26 && dir[entry + offset] != '\0'; offset++, s++);
+        for (offset = 6, s = 0; offset < 26 && dir[entry + offset] != TERM; offset++, s++)
+            ;
 
         print(filename);
         print(" (");
-        if (s >= 10) {
+        if (s >= 10)
+        {
             sizeStr[0] = (s / 10) + '0';
         }
         sizeStr[0 + (s >= 10)] = mod(s, 10) + '0';
-        sizeStr[1 + (s >= 10)] = '\0';
+        sizeStr[1 + (s >= 10)] = TERM;
         print(sizeStr);
-        if (s > 1) {
+        if (s > 1)
+        {
             printLn(" sectors)");
-        } else {
+        }
+        else
+        {
             printLn(" sector)");
         }
-        
     }
-
 }
 
-void del(char *filename) {
+void del(char *filename)
+{
     syscall(7, filename, 0, 0);
 }
 
-void copy(char *args) {
+void copy(char *args)
+{
     char from[7];
     char to[7];
     int n;
@@ -226,43 +240,52 @@ void copy(char *args) {
 
     for (; n < 7; n++)
     {
-        from[n] = '\0';
+        from[n] = TERM;
     }
 
-    for (n = 0, args++; *args != '\0'; n++, args++)
+    for (n = 0, args++; *args != TERM; n++, args++)
     {
         to[n] = *args;
     }
 
     for (; n < 7; n++)
     {
-        to[n] = '\0';
+        to[n] = TERM;
     }
 
     syscall(3, from, buf, &sectors);
 
-    if (sectors > 0) {
+    if (sectors > 0)
+    {
         syscall(8, buf, to, sectors);
-    } else {
+    }
+    else
+    {
         print("Cannot copy '");
         print(from);
         print("': No such file");
     }
 }
 
-void create(char *filename) {
+void create(char *filename)
+{
     char file[13312];
     char input[13312];
     char *filePtr = file;
     int i, bSize, sectors = 0;
 
-    while (bSize < 13311) {
+    while (bSize < 13311)
+    {
         print("... ");
         readLn(input);
 
-        if (input[0] == '\r') { break; }
+        if (input[0] == '\r')
+        {
+            break;
+        }
 
-        for (i = 0; input[i] != TERM && bSize < 13311; i++, filePtr++, bSize++) {
+        for (i = 0; input[i] != TERM && bSize < 13311; i++, filePtr++, bSize++)
+        {
             *filePtr = input[i];
         }
     }
@@ -278,7 +301,8 @@ void create(char *filename) {
      *      + 1 if there is a remainder
      */
     sectors = bSize == 0 ? 1 : bSize / 512;
-    if (mod(bSize, 512) != 0) sectors++;
+    if (mod(bSize, 512) != 0)
+        sectors++;
 
     syscall(8, file, filename, sectors);
 }
