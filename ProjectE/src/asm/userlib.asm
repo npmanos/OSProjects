9c91bcfb436c1d861dd86114e02527cab71d29e3
Nick Manos <nmanos@student.bridgew.edu>
2021-12-21 21:03:47 -0500

    Update asm files and add c files from directions

--- ProjectE/src/asm/userlib.asm
+++ ProjectE/src/asm/userlib.asm
@@ -4,6 +4,7 @@
 ;userlib.asm contains assembly functions for user programs such as shell
 
 	.global _syscall
+	.global _enableInterrupts
 
 ;void syscall (int AX, int BX, int CX, int DX)
 _syscall:
@@ -15,4 +16,10 @@ _syscall:
 	mov dx,[bp+10]
 	int #0x21
 	pop bp
+	ret
+
+;void enableInterrupts()
+;call at the beginning of programs.  allows timer preemption
+_enableInterrupts:
+	sti
 	ret
